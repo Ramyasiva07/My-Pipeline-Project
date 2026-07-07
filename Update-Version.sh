13c4f4bce3f72f109cf791fab99214b441ee0a64
Ramya <ramyasiva058@gmail.com>
2026-07-07 13:53:33 +0530

    fix 7

--- Update-Version.sh
+++ Update-Version.sh
@@ -10,7 +10,7 @@ echo "Current NGINX version deployed: $CURRENT_TAG"
 
 echo "=== Step 2: Set the new version ==="
 
-NEW_TAG="1.27.6"
+NEW_TAG="1.27.5"
 
 echo "New NGINX version to deploy: $NEW_TAG"
 
